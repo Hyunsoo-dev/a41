06e2bad3ae967c753bbc0dcd295d26d91acdbbe6
선종권 <jonggwon@opusm.io>
2022-02-19 16:43:57 +0900

    change center box

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,18 +4,23 @@ import  LinkedIn  from "../Assets/linkedin.svg";
 import Twitter from '../Assets/twitter.svg';
 import LogoFooter from '../Assets/logoFooter.svg';
 
-const Footer = () => {
+type FooterProps = {
+    dark : boolean;
+}
+
+const Footer = ({ dark } : FooterProps) => {
     const handleClickLinkedIn = () => {
         return;
     }
 
     const handleClickTwitter = () => {
-
+        return;
     }
 
     return (
-        <div className="footer-container">
-            <div className="footer-frame3">
+        <div className={dark ? "footer-container-dark" : "footer-container"}>
+            <div className="content-box">
+                <div className="footer-frame3">
                 <img src={LogoFooter} alt={"logoFooter"} className={"logo-footer"}/>
                 <div className={"footer-frame2"}>
                     <div className={"icon-container"}>
@@ -29,6 +34,7 @@ const Footer = () => {
                         Copyright 2021.a41ventures all rights reserved.
                     </p>
                 </div>
+            </div>
             </div>
         </div>
     )
