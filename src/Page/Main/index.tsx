ed306634230b808f6018ede182406a9611d63247
Hyunsoo-dev <jing07161@gmail.com>
2022-02-22 20:56:20 +0900

    WIP: main page

--- src/Page/Main/index.tsx
+++ src/Page/Main/index.tsx
@@ -2,108 +2,112 @@ import React from "react";
 import "../../Style/Main.scss";
 const Main = () => {
   return (
-    <section id="container">
-      <section className="main_title">
-        <div className="main_title_wrapper">
-          <article className="title">We are the sidekicks of heroes</article>
-          <article className="content">building digital innovation</article>
-        </div>
-      </section>
-      <section className="main_ventures">
-        <div className="main_ventures_wrapper">
-          <article>a41 Ventures</article>
-          <article>
-            a41 invests in disruptive technologies that will lead the next digital innovation. We believe that those who do the
-            heavy lifting on the ground - members of the projects that we invest in - are the true heroes that should stand out,
-            with the help of sidekicks like us. Members of a41 are dedicated to becoming the bridge and catalyst of the next
-            digital innovation.
-          </article>
-        </div>
-      </section>
-      <section className="main_ourThesis">
-        <div className="main_ourThesis_wrapper">
-          <article>our thesis</article>
-          <div>
-            <article>Winning the next era of</article>
-            <article>internet - Web 3, </article>
-            <article>Winning the next era of</article>
-            <article>finance - Open Finance</article>
-            <article>Winning the next era of</article>
-            <article>digital experience - Metaverse</article>
+    <section className="container_bg">
+      <section className="container">
+        <section className="main_title">
+          <div className="main_title_wrapper">
+            <article className="title">We are the sidekicks of heroes</article>
+            <article className="content">building digital innovation</article>
           </div>
-        </div>
-      </section>
-      <section className="main_portfolio">
-        <div className="main_portfolio_wrapper">
-          <article>portfolio</article>
-          <div className="portfolio_box">
-            <article className="portfolio_logo"></article>
-            <article className="portfolio_logo"></article>
-            <article className="portfolio_logo"></article>
-            <article className="portfolio_logo"></article>
-            <article className="portfolio_logo"></article>
+        </section>
+        <section className="main_ventures">
+          <div className="main_ventures_wrapper">
+            <article>a41 Ventures</article>
+            <article>
+              a41 invests in disruptive technologies that will lead the next
+              digital innovation. We believe that those who do the heavy lifting
+              on the ground - members of the projects that we invest in - are
+              the true heroes that should stand out, with the help of sidekicks
+              like us. Members of a41 are dedicated to becoming the bridge and
+              catalyst of the next digital innovation.
+            </article>
           </div>
-        </div>
-      </section>
-      <section className="main_contents">
-        <div className="main_contents_wrapper">
-          <article></article>
-          <article></article>
-          <article></article>
-          <article></article>
-        </div>
-      </section>
-      <section className="main_team">
-        <div className="main_team_wrapper">
-          <article>
+        </section>
+        <section className="main_ourThesis">
+          <div className="main_ourThesis_wrapper">
+            <article>our thesis</article>
             <div>
-              <img src="" />
+              <article>Winning the next era of</article>
+              <article>internet - Web 3, </article>
+              <article>Winning the next era of</article>
+              <article>finance - Open Finance</article>
+              <article>Winning the next era of</article>
+              <article>digital experience - Metaverse</article>
             </div>
-            <div>
-              <div>
-                <div>John Park</div>
-                <div>박광성</div>
-              </div>
-              <div>Co-Founder | Managing Partner</div>
-              <div>
-                <div>새</div>
-                <div>in</div>
-              </div>
+          </div>
+        </section>
+        <section className="main_portfolio">
+          <div className="main_portfolio_wrapper">
+            <article>portfolio</article>
+            <div className="portfolio_box">
+              <article className="portfolio_logo"></article>
+              <article className="portfolio_logo"></article>
+              <article className="portfolio_logo"></article>
+              <article className="portfolio_logo"></article>
+              <article className="portfolio_logo"></article>
             </div>
-          </article>
-          <article>
-            <div>
-              <img src="" />
-            </div>
-            <div>
+          </div>
+        </section>
+        <section className="main_contents">
+          <div className="main_contents_wrapper">
+            <article></article>
+            <article></article>
+            <article></article>
+            <article></article>
+          </div>
+        </section>
+        <section className="main_team">
+          <div className="main_team_wrapper">
+            <article>
               <div>
-                <div>John Park</div>
-                <div>박광성</div>
+                <img src="" />
               </div>
-              <div>Co-Founder | Managing Partner</div>
               <div>
-                <div>새</div>
-                <div>in</div>
+                <div>
+                  <div>John Park</div>
+                  <div>박광성</div>
+                </div>
+                <div>Co-Founder | Managing Partner</div>
+                <div>
+                  <div>새</div>
+                  <div>in</div>
+                </div>
               </div>
-            </div>
-          </article>
-          <article>
-            <div>
-              <img src="" />
-            </div>
-            <div>
+            </article>
+            <article>
               <div>
-                <div>John Park</div>
-                <div>박광성</div>
+                <img src="" />
               </div>
-              <div>Co-Founder | Managing Partner</div>
               <div>
-                <div>새</div>
-                <div>in</div>
+                <div>
+                  <div>John Park</div>
+                  <div>박광성</div>
+                </div>
+                <div>Co-Founder | Managing Partner</div>
+                <div>
+                  <div>새</div>
+                  <div>in</div>
+                </div>
               </div>
-            </div>
-          </article>
-        </div>
+            </article>
+            <article>
+              <div>
+                <img src="" />
+              </div>
+              <div>
+                <div>
+                  <div>John Park</div>
+                  <div>박광성</div>
+                </div>
+                <div>Co-Founder | Managing Partner</div>
+                <div>
+                  <div>새</div>
+                  <div>in</div>
+                </div>
+              </div>
+            </article>
+          </div>
+        </section>
       </section>
     </section>
   );
