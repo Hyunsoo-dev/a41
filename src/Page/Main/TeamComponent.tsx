3a57380a96ac54e99a149f549eb52b1b81ed8f0b
Hyunsoo-dev <jing07161@gmail.com>
2022-02-25 18:06:21 +0900

    WIP: main page/ team section 구현중

--- src/Page/Main/TeamComponent.tsx
+++ src/Page/Main/TeamComponent.tsx
@@ -1,5 +1,24 @@
 import React from "react";
-const TeamComponent = ({ thumbnail, enName, koName }) => {
-  return <></>;
+const TeamComponent = ({ thumbnail, enName, koName, position, sns }: any) => {
+  return (
+    <section className="team-container">
+      <section className="team-column1">
+        <img src={thumbnail} />
+      </section>
+      <section className="team-column2">
+        <section className="name-box">
+          <article className="enName">{enName}</article>
+          <article className="koName">{koName}</article>
+        </section>
+        <section className="job-box">
+          <article>{position}</article>
+        </section>
+        <section className="sns-box">
+          <article className="sns-logo">logo</article>
+          <article className="sns-name">in</article>
+        </section>
+      </section>
+    </section>
+  );
 };
 export default TeamComponent;
--- src/Page/Main/index.tsx
+++ src/Page/Main/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "../../Style/Main.scss";
 import ContentComponent from "./ContentComponent";
+import TeamComponent from "./TeamComponent";
 import column1_row1 from "../../Assets/image/mainPage/column1_row1.png";
 import { ReactComponent as AlexLogo } from "../../Assets/image/mainPage/alexLogo.svg";
 import { ReactComponent as ApricotLogo } from "../../Assets/image/mainPage/apricotLogo.svg";
 import { ReactComponent as ArkadikoLogo } from "../../Assets/image/mainPage/arkadikoLogo.svg";
 import { ReactComponent as BlockSurveLogo } from "../../Assets/image/mainPage/blockSurveLogo.svg";
 import { ReactComponent as BribeLogo } from "../../Assets/image/mainPage/bribeLogo.svg";
+// import snsLogo from "../../Assets/image/mainPage/sns.svg";
+import { ReactComponent as SnsLogo } from "../../Assets/image/mainPage/sns.svg";
+import team_thumbnail from "../../Assets/image/mainPage/team_thumbnail.png";
 const Main = () => {
   return (
     <>
@@ -14,7 +18,9 @@ const Main = () => {
         <section className="container-bg">
           <section className="main-title">
             <div className="main-title-wrapper">
-              <article className="title">We are the sidekicks of heroes</article>
+              <article className="title">
+                We are the sidekicks of heroes
+              </article>
               <article className="title">building digital innovation</article>
             </div>
           </section>
@@ -26,10 +32,12 @@ const Main = () => {
             <div className="main-ventures-wrapper">
               <article className="title">a41 Ventures</article>
               <article className="content">
-                a41 invests in disruptive technologies that will lead the next digital innovation. We believe that those who do
-                the heavy lifting on the ground - members of the projects that we invest in - are the true heroes that should
-                stand out, with the help of sidekicks like us. Members of a41 are dedicated to becoming the bridge and catalyst of
-                the next digital innovation.
+                a41 invests in disruptive technologies that will lead the next
+                digital innovation. We believe that those who do the heavy
+                lifting on the ground - members of the projects that we invest
+                in - are the true heroes that should stand out, with the help of
+                sidekicks like us. Members of a41 are dedicated to becoming the
+                bridge and catalyst of the next digital innovation.
               </article>
             </div>
           </section>
@@ -41,16 +49,26 @@ const Main = () => {
             <div className="main-ourThesis-wrapper">
               <article className="title">our thesis</article>
               <div className="content-box">
-                <article className="dark-content">Winning the next era of</article>
+                <article className="dark-content">
+                  Winning the next era of
+                </article>
                 <article className="light-content">internet - Web 3, </article>
               </div>
               <div className="content-box">
-                <article className="dark-content">Winning the next era of</article>
-                <article className="light-content">finance - Open Finance</article>
+                <article className="dark-content">
+                  Winning the next era of
+                </article>
+                <article className="light-content">
+                  finance - Open Finance
+                </article>
               </div>
               <div className="content-box">
-                <article className="dark-content">Winning the next era of</article>
-                <article className="light-content">digital experience - Metaverse</article>
+                <article className="dark-content">
+                  Winning the next era of
+                </article>
+                <article className="light-content">
+                  digital experience - Metaverse
+                </article>
               </div>
             </div>
           </section>
@@ -92,14 +110,18 @@ const Main = () => {
                   <ContentComponent
                     thumbnail={column1_row1}
                     title={"[Research] Pricing Everlasting Options"}
-                    subtitle={"This post explores ways to price everlasting options."}
+                    subtitle={
+                      "This post explores ways to price everlasting options."
+                    }
                     author={"Steve Kim"}
                     date={"Dec 24, 2021"}
                   />
                   <ContentComponent
                     thumbnail={column1_row1}
                     title={"[Research] Pricing Everlasting Options"}
-                    subtitle={"This post explores ways to price everlasting options."}
+                    subtitle={
+                      "This post explores ways to price everlasting options."
+                    }
                     author={"Steve Kim"}
                     date={"Dec 24, 2021"}
                   />
@@ -108,14 +130,18 @@ const Main = () => {
                   <ContentComponent
                     thumbnail={column1_row1}
                     title={"[Research] Pricing Everlasting Options"}
-                    subtitle={"This post explores ways to price everlasting options."}
+                    subtitle={
+                      "This post explores ways to price everlasting options."
+                    }
                     author={"Steve Kim"}
                     date={"Dec 24, 2021"}
                   />
                   <ContentComponent
                     thumbnail={column1_row1}
                     title={"[Research] Pricing Everlasting Options"}
-                    subtitle={"This post explores ways to price everlasting options."}
+                    subtitle={
+                      "This post explores ways to price everlasting options."
+                    }
                     author={"Steve Kim"}
                     date={"Dec 24, 2021"}
                   />
@@ -130,53 +156,28 @@ const Main = () => {
           <section className="main-team">
             <div className="main-team-wrapper">
               <article className="title">Team</article>
-              <article>
-                <div>
-                  <img src="" />
-                </div>
-                <div>
-                  <div>
-                    <div>John Park</div>
-                    <div>박광성</div>
-                  </div>
-                  <div>Co-Founder | Managing Partner</div>
-                  <div>
-                    <div>새</div>
-                    <div>in</div>
-                  </div>
-                </div>
-              </article>
-              <article>
-                <div>
-                  <img src="" />
-                </div>
-                <div>
-                  <div>
-                    <div>John Park</div>
-                    <div>박광성</div>
-                  </div>
-                  <div>Co-Founder | Managing Partner</div>
-                  <div>
-                    <div>새</div>
-                    <div>in</div>
-                  </div>
-                </div>
-              </article>
-              <article>
-                <div>
-                  <img src="" />
-                </div>
-                <div>
-                  <div>
-                    <div>John Park</div>
-                    <div>박광성</div>
-                  </div>
-                  <div>Co-Founder | Managing Partner</div>
-                  <div>
-                    <div>새</div>
-                    <div>in</div>
-                  </div>
-                </div>
+              <article className="main-team-box">
+                <TeamComponent
+                  thumbnail={team_thumbnail}
+                  enName="John Park"
+                  koName="박광성"
+                  position="Co-Founder | Managing Partner"
+                  sns={SnsLogo}
+                />
+                <TeamComponent
+                  thumbnail={team_thumbnail}
+                  enName="John Park"
+                  koName="박광성"
+                  position="Co-Founder | Managing Partner"
+                  sns={SnsLogo}
+                />
+                <TeamComponent
+                  thumbnail={team_thumbnail}
+                  enName="John Park"
+                  koName="박광성"
+                  position="Co-Founder | Managing Partner"
+                  sns={SnsLogo}
+                />
               </article>
             </div>
           </section>
