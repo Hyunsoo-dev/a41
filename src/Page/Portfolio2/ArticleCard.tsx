8ea838a769d219baad21fee9f3e1ad7666d1f8cb
sosick0418 <sosick0418@gmail.com>
2022-05-28 17:02:22 +0900

    link to team article and content

--- src/Page/Portfolio2/ArticleCard.tsx
+++ src/Page/Portfolio2/ArticleCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 import '../../Style/ArticleCard.scss'
 
 
@@ -12,18 +13,23 @@ type ArticleCardProps = {
 }
 
 const ArticleCard = ({img, link, title, summary, author, date}: ArticleCardProps) => {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate(`../../content/${link}`)
+    }
+
     return <div className={"card_box"}>
-        <a href={link}>
+        <div onClick={handleClick}>
             <img src={img} alt={"article"}/>
             <div className={"card_title"}>{title}</div>
             <div className={"card_summary"}>{summary}</div>
             <div className={"author_box"}>
                 <span>Author</span>
                 <span>{author}</span>
-                <span>.</span>
+                <span>&#183;</span>
                 <span>{date}</span>
             </div>
-        </a>
+        </div>
     </div>
 
 }
